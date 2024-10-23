a858505db050117d20afef69025cb2cbd2a63aea
中里駿太 <shunta-24-solar@outlook.jp>
2024-10-23 19:13:35 +0900

    manifest.json追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <RichResults />
       </head>
 
