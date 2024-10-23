5a768bb7809559b2de95dae09bb4b09dd55d4f32
中里駿太 <shunta-24-solar@outlook.jp>
2024-10-23 19:33:23 +0900

    config修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["imgfp.hotp.jp"],
+    // NOTE:外部から画像を読み込む際には対象のURLを記載する
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imgfp.hotp.jp",
+      },
+    ],
   },
-  reactStrictMode:false
+  reactStrictMode: false,
 };
 
 export default nextConfig;
